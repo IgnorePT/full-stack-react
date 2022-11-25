c086a42a05034b149d3de3a7568308b632eb32ee
netluigi <luiz.anfe@hotmail.com>
2022-11-25 22:54:32 +0000

    feito

---
+++ fundamentals/ex01.js
@@ -0,0 +1,8 @@
+
+
+let createDiv = document.createElement('div')
+let text = document.createTextNode('Hello Edit')
+
+document.body.append(createDiv)
+createDiv.append(text)
+divHtml.appendChild(createDiv)
