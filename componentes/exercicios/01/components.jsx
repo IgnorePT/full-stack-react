6e32a07031df2e88aff894ed5dc3d98ccbf06314
Alunos <alunos@MacBook-Air-de-EDIT.local>
2022-11-28 22:13:01 +0000

    react ex

---
+++ componentes/exercicios/01/components.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+const FunctionalComponent = () =>  {
+
+    const title = 'Copa do Mundo da FIFA 2022™ '
+
+    return (
+    <div>
+        <div>
+        {title}
+        </div>
+        
+    </div>
+)
+}
