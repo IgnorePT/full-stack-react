21540d8fb4edb99407eb7c4b3c811a649adaceec
nelsonfernandes-kk <nelson.fernandes@kk.pt>
2022-12-02 18:58:09 +0000

    Conditional Rendering & Exercicios

---
+++ componentes/exercicios/Extra 02/ex7_extra_componentes.js
@@ -0,0 +1,11 @@
+  /** 
+   * Exercicio 07 Extra: Movie app
+   * 
+    * Cria o design presente no ficheiro do Figma em React: 
+   *  - Exercicio Movies App.fig
+   *  - Online: https://www.figma.com/file/bhA7BXARgzvakuhIGSI19f/Exercicio---Movies-App?node-id=0%3A1&t=EdPH48zjAFdDNNHX-1
+   * 
+    * Recursos:
+   *  - Aplicações: Figma (https://www.figma.com)
+   *  - Fonts: Poppins - https://fonts.google.com/specimen/Poppins?query=Poppins#styles
+   */
