196bcbe6dd8094aadc58fd70e5fc9604ad538284
ctxmlk <catiasrl_22@hotmail.com>
2022-12-05 21:33:45 +0000

    atualização ex05

--- componentes/exercicios/05/ex05_listagemArrays.js
+++ componentes/exercicios/05/ex05_listagemArrays.js
@@ -1,16 +1,16 @@
+//-----------O projeto não está inicializado, não tem node modules, etc... ver enunciado de novo e inicializar (dicas em baixo);
+
 /**
  * Spotify Fav
- * 
+ *
  * Ex 01:
  * Cria uma aplicação que receba um array das tuas musicas favoritas
  * e as as aplique numa lista segue a lista em exemplo
- * 
+ *
  * Ex 02:
- * Cria a mesma aplicação com recursoa um array de objetos 
+ * Cria a mesma aplicação com recursoa um array de objetos
  * Cria uma lista de cards com Titulo, Artista, Imagem
  */
-
-const favList = ["musica 1", "musica 2"];
 
 // HTML esperado
 /**
@@ -23,38 +23,43 @@ const favList = ["musica 1", "musica 2"];
  * </div>
  */
 
+import './App.css';
 
-//Ex 02
-// Estrutura do Array
-const favListObject = [{
-    idMusica: 0,
-    nome: "",
-    artista: ""
-}];
+const SpotifyList = (props) => {
+	console.log(props.title);
+	//ou
+	const { title, musicList } = props;
+	console.log(title);
+	return (
+		<div className="spotify-fav">
+			<h3>{title}</h3>
+			<ul className="music-list">
+				{musicList.map((music, index) => {
+					const id = uuidv4();
+					return (
+						<li key={id} className="music">
+							{music}
+						</li>
+					);
+				})}
+			</ul>
+		</div>
+	);
+};
+//Para as keys unicas nas li, instalar package uuid, código já escrito  OU crypto.randomUUID()
+function App() {
+	const favList = ['musica 1', 'musica 2', 'musica 3', 'musica 4'];
 
-// HTML esperado
-/**
- * <div class="spotify-card-list">
- *  <div class="music-card">
- *      <img src="" />
- *      <h3 class="title">*nome musica*</div>
- *      <h5 class="artist">*nome do artista</div>
- *  </div>
- * *  <div class="music-card">
- *      <img src="" />
- *      <h3 class="title">*nome musica*</div>
- *      <h5 class="artist">*nome do artista</div>
- *  </div>
- *  ...
- * </div>
- */
+	return <SpotifyList title="Coisa" musicList={favList}></SpotifyList>;
+}
+
+export default App;
 
 /**
  * Dicas:
  *  - Podes criar uma aplicação num projeto novo com recursos Vite ou CRA
  *  - Podes criar a const favListObject no ficheiro App.jsx (vite) | App.js (cra)
- *  - Para passares o variavel cria uma chave e passa o faviListObject 
+ *  - Para passares o variavel cria uma chave e passa o faviListObject
  *    Passar para filho: <Componente favList={*variavel*} />
- *    Usar no componente: const Componente = (props) => {...} 
+ *    Usar no componente: const Componente = (props) => {...}
  */
-
