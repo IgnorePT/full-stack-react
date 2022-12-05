9e9568f14577067da9fd38b045e71eea8b88e314
netluigi <luiz.anfe@hotmail.com>
2022-12-05 19:10:30 +0000

    att

--- componentes/exercicios/02/ex2_componentes.js
+++ componentes/exercicios/02/ex2_componentes.js
@@ -1,23 +1,32 @@
-  /*
-        Cria o teu primeiro componente em React dentro da tua aplicação com recurso a Toolchain
+
+        // Cria o teu primeiro componente em React dentro da tua aplicação com recurso a Toolchain
        
-        Cria um componente chamado App que apresente a seguinte estrutura
-        <div class="main">
-          <h2>Copa do Mundo da FIFA 2022™ </h2>
+        // Cria um componente chamado App que apresente a seguinte estrutura
 
-          <div class="jogo">
-            <div class="club">
-              <h3 > Portugal </h3>
-              <h5 class="golos"> 1 </h5>
-            </div>
+  const Results = (props) =>{
+    return(
+      <div className='club'>
+        <h3>
+          {props.name}
+        </h3>
+        <h5>
+          {props.gol}
+        </h5>
 
-            <div class="club">
-              <h3> Uruguai </h3>
-              <h5 class="golos"> 0 </h5>
-            </div>
-          </div>
+      </div>
+    )
+
+  }
+
+  < div class="main" >
+      <h2>Copa do Mundo da FIFA 2022™ </h2>
+
+      <div class="jogo">
+        <Results name='Portugal' gol='0' />
+        <Results name='Brasil' gol='3' />
+
+      </div>
           
-        </div>
+  </div >
 
-        Extra: Tenta tornar o a div dos clubes em componentes e importa-os dentro da App
-      */
+  // Extra: Tenta tornar o a div dos clubes em componentes e importa - os dentro da App
