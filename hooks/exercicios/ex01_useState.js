1206f7468dee4d9f1201e32a96466fb3cb5ee331
Marco Rodrigues <marcoaurelio853@gmail.com>
2022-12-10 12:50:03 +0000

    merge

--- hooks/exercicios/ex01_useState.js
+++ hooks/exercicios/ex01_useState.js
@@ -1,14 +1,17 @@
 /**
- * 
+ *
  * Cria uma counter APP cujo componente modifique um estado
  * e aplique o seu estado ao Titulo do projeto
- * 
- * Dicas: 
+ *
+ * Dicas:
  * - Armazenem o counter num estado useState
  * - Não se esqueçam de importar
  * - Modifiquem no onClick
  * - Podem usar https://codesandbox.io/
  */
+
+
+// https://codesandbox.io/p/sandbox/magical-varahamihira-rzqq04?file=%2Fsrc%2FApp.tsx&selection=%5B%7B%22endColumn%22%3A1%2C%22endLineNumber%22%3A19%2C%22startColumn%22%3A1%2C%22startLineNumber%22%3A19%7D%5D&workspace=%257B%2522activeFileId%2522%253A%2522cl9zkf3e9000alqiphi669wk8%2522%252C%2522openFiles%2522%253A%255B%2522%252FREADME.md%2522%252C%2522%252Fsrc%252FApp.tsx%2522%255D%252C%2522sidebarPanel%2522%253A%2522EXPLORER%2522%252C%2522gitSidebarPanel%2522%253A%2522COMMIT%2522%252C%2522spaces%2522%253A%257B%2522clbbbf4go000u3b6jemgsicog%2522%253A%257B%2522key%2522%253A%2522clbbbf4go000u3b6jemgsicog%2522%252C%2522name%2522%253A%2522Default%2520Space%2522%252C%2522devtools%2522%253A%255B%257B%2522type%2522%253A%2522PREVIEW%2522%252C%2522taskId%2522%253A%2522dev%2522%252C%2522port%2522%253A5173%252C%2522key%2522%253A%2522clbbbf4go000v3b6jliwwdi7j%2522%252C%2522isMinimized%2522%253Afalse%257D%255D%257D%257D%252C%2522currentSpace%2522%253A%2522clbbbf4go000u3b6jemgsicog%2522%257D
 
 const Componente = () => {
 
