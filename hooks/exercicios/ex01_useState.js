885078fe98d64ae678e3c86266f6211f806aacbb
mariamoniztome <mariatomejobs@gmail.com>
2022-12-11 02:49:04 +0000

    #useState

--- hooks/exercicios/ex01_useState.js
+++ hooks/exercicios/ex01_useState.js
@@ -1,19 +1,52 @@
 /**
- * 
+ *
  * Cria uma counter APP cujo componente modifique um estado
  * e aplique o seu estado ao Titulo do projeto
- * 
- * Dicas: 
+ *
+ * Dicas:
  * - Armazenem o counter num estado useState
  * - Não se esqueçam de importar
  * - Modifiquem no onClick
  * - Podem usar https://codesandbox.io/
  */
 
-const Componente = () => {
+ //Link sandbox: https://codesandbox.io/p/sandbox/gifted-https-lkt4rj?file=%2Fsrc%2FApp.tsx
 
-    return <div onClick={() => {
-    }}
+function App() {
+  const [state, setSate] = useState(0);
 
-    //onChange, onInput
+  return (
+    <>
+      <h1>Adiciona de 0-10</h1>
+      <h2>{state}</h2>
+      <button
+        onClick={() =>
+          setSate((contadorInicial) => {
+            if (contadorInicial >= 0 && contadorInicial < 10) {
+              return contadorInicial + 1;
+            }
+
+            alert("Não pode haver números maiores que 10");
+
+            return 0;
+          })
+        }
+      >
+        Add
+      </button>
+      <button
+        onClick={() =>
+          setSate((contadorInicial) => {
+            if (contadorInicial > 0 && contadorInicial < 10) {
+              return contadorInicial - 1;
+            }
+            alert("Não pode haver números negativos");
+            return 0;
+          })
+        }
+      >
+        Remove
+      </button>
+    </>
+  );
 }
--- hooks/exercicios/ex02_useState.js
+++ hooks/exercicios/ex02_useState.js
@@ -7,29 +7,6 @@
  * - Para remover ou adicionar elementos devemos modificar o estado
  */
 
- const initialTodos = [
-    {
-      id: "a",
-      task: "Learn React",
-      complete: true
-    },
-    {
-      id: "b",
-      task: "Learn NodeJS",
-      complete: true
-    },
-    {
-      id: "c",
-      task: "Learn MongoDB",
-      complete: false
-    }
-  ];
-
-  const elementoAdicionar = {
-    id: "d",
-    task: "Master JS",
-    complete: false
-  }
 
   /**
    * Estrutura
@@ -50,3 +27,78 @@
    */
 
   
+//Link: https://codesandbox.io/p/github/codesandbox/codesandbox-template-vite-react/csb-khrm2r/draft/muddy-surf?file=%2Fsrc%2FApp.tsx&selection=%5B%7B%22endColumn%22%3A37%2C%22endLineNumber%22%3A50%2C%22startColumn%22%3A37%2C%22startLineNumber%22%3A50%7D%5D
+
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+
+const initialTodos = [
+  {
+    id: "a",
+    task: "Learn React",
+    complete: true,
+  },
+  {
+    id: "b",
+    task: "Learn NodeJS",
+    complete: true,
+  },
+  {
+    id: "c",
+    task: "Learn MongoDB",
+    complete: false,
+  },
+];
+
+const elementoAdicionar = {
+  id: "d",
+  task: "Master JS",
+  complete: false,
+};
+
+const App = () => {
+  const [task, setTask] = useState("");
+  const [todos, setTodos] = useState(initialTodos);
+
+  //console.log(task);
+  //console.log(useState);
+
+  const changeStatus = (event) => {
+    setTask(event.target.value);
+  };
+  const handleSubmit = (event) => {
+    //alert("teste");
+    if (task) {
+      setTodos(todos.concat({ id: "d", task: "Master JS", complete: false }));
+    }
+    setTask(" ");
+    //event.preventDefault();
+  };
+
+  const handleChange = (event) => {
+    //alert("teste");
+    if (task) {
+      alert("Não consigo pensar mais");
+    }
+
+    //event.preventDefault();
+  };
+
+  return (
+    <div className="learn-list">
+      <div className="title">
+        <h1>My Roadmap</h1>
+      </div>
+      <ul>
+        {todos.map((todo) => (
+          <li key={todo.id}>{todo.task}</li>
+        ))}
+      </ul>
+
+      <button onClick={handleSubmit}>Add Task</button>
+      <button onClick={handleChange}>Remove Task</button>
+    </div>
+  );
+};
+
+export default App;
