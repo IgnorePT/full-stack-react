375f84b3d4354d8b8892c201a2253b68762ee911
smag-dev <sergiomagomes@gmail.com>
2022-12-24 17:57:29 +0000

    adicionado exercicios de hooks

---
+++ hooks/exercicios/ListaArray02.jsx
@@ -0,0 +1,24 @@
+const musics = (favListObject) => { 
+  const listMusics = favListObject.map(music => {
+    return (
+      <div className="music-card" key={music.idMusica}>
+          <img width="100" height= "100"src={music.img} />
+          <h3 className="title">{music.nome}</h3>
+          <h5 className="artist">{music.artista}</h5>
+      </div>
+      )
+  })
+
+  return listMusics 
+}
+
+const ListaArray02 = ({favListObject}) => {
+
+      return (
+          <div className="spotify-card-list">
+            {musics(favListObject)}
+          </div>
+      );
+    };
+    
+    export default ListaArray02;
